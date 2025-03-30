e4bb05c036477b0afaff75eaada280340b476cc0
krystalfloraa <130507211+krystalfloraa@users.noreply.github.com>
2025-03-30 18:07:29 -0500

    Add files via upload

---
+++ story_content/6ewSY6ceovg_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.733%0D%0AFirst,%20remove%20all%20your%20jewelry%20and%20turn%20on%20the%20faucet%20to%20run%20warm%20water.%0D%0A%0D%0A"}')
