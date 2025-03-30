facb5b6c407e12b38d79c009caeb46df0af6272a
krystalfloraa <130507211+krystalfloraa@users.noreply.github.com>
2025-03-30 18:35:21 -0500

    Add files via upload

---
+++ story_content/6Pclg8ObNfH_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.711%0D%0AAnd%20lastly,%20how%20long%20should%20you%20wash%20your%20hands%20for?%0D%0A%0D%0A"}')
---
+++ story_content/6nRegKRHkal_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34263.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.526%0D%0AMmmm,%20not%20quite.%20Let%E2%80%99s%20try%20that%20again.%0D%0A%0D%0A"}')
